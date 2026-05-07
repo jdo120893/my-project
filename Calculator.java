415e7788c50e3893568e5539a83300fa3941bd02
jdo120893 <jdo120893@gmail.com>
2026-05-07 23:21:16 +0900

    Delete Calculator.java

--- Calculator.java
+++
@@ -1,8 +0,0 @@
-public class Calculator {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int sum = a+b;
-        System.out.println("10 + 20 = " + sum);
-    }
-}
